03e9f4d79c5dd910d2cd6f57d791a88130b6505a
"Shane Holloway ext:(%22) <shane.holloway@ieee.org>
2008-02-09 16:40:37 -0700

    Added webkit ctrl

---
+++ proj/skinning/toolkits/wx/external/webkit.py
@@ -0,0 +1,54 @@
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+##~ Copyright (C) 2002-2007  TechGame Networks, LLC.              ##
+##~                                                               ##
+##~ This library is free software; you can redistribute it        ##
+##~ and/or modify it under the terms of the BSD style License as  ##
+##~ found in the LICENSE file included with this distribution.    ##
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~ Imports 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+from TG.skinning.toolkits.wx._baseElements import *
+import wx.webkit 
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~ Definitions 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+class webkit(wxPyWidgetSkinElement):
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #~ Constants / Variables / Etc. 
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    elementGlobals = wxPyWidgetSkinElement.elementGlobals.copy()
+    elementGlobals.update(vars(wx.webkit ))
+
+    defaultSettings = wxPyWidgetSkinElement.defaultSettings.copy()
+    defaultSettings.update({ 
+        })
+
+    defaultStyleSettings = wxPyWidgetSkinElement.defaultStyleSettings.copy()
+    defaultStyleSettings.update({
+        'layout-cfg': '1,EXPAND',
+        'ref': 'http://www.python.org',
+
+        })
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #~ Definitions 
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def createWidget(self, parentObj):
+        kwWX = self.getStyleSettingsCollectionWX(['style', 'pos', 'size'], ['ref'])
+        kwWX.rename(strURL='ref', winID='id')
+        obj = wx.webkit.WebKitCtrl(parentObj, **kwWX)
+        return obj
+
+    def finishWidget(self, obj, parentObj):
+        pass
+
+    def installDefaultEvent(self, evtHandler, evtObject, evtCallback):
+        wx.webkit.EVT_WEBKIT_STATE_CHANGED(evtHandler, evtCallback)
+
