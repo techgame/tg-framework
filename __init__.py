ecb8da6fbd36950cf8c864ec521449fdc280a775
"Shane Holloway ext:(%22) <shane.holloway@ieee.org>
2009-01-20 15:45:29 -0700

    Updated copyright

--- __init__.py
+++ __init__.py
@@ -1,10 +1,7 @@
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##~ Copyright (C) 2002-2004  TechGame Networks, LLC.
-##~ 
-##~ This library is free software; you can redistribute it and/or
-##~ modify it under the terms of the BSD style License as found in the 
-##~ LICENSE file included with this distribution.
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-# Generic TG package file
-
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+##~ Copyright (C) 2002-2009  TechGame Networks, LLC.              ##
+##~                                                               ##
+##~ This library is free software; you can redistribute it        ##
+##~ and/or modify it under the terms of the BSD style License as  ##
+##~ found in the LICENSE file included with this distribution.    ##
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
